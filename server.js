e6607c5cd125ba83772136ae7559b2a14832f0be
ctjoy <ctjoychen@gmail.com>
2016-09-29 15:41:39 +0800

    finish html css

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.get('/cv.html', function (req, res) {
+  res.sendFile(path.join(__dirname+'/cv.html'));
+});
+
+app.get('/research.html', function (req, res) {
+  res.sendFile(path.join(__dirname+'/research.html'));
+});
+
+app.get('/projects.html', function (req, res) {
+  res.sendFile(path.join(__dirname+'/projects.html'));
+});
+
+app.get('/publications.html', function (req, res) {
+  res.sendFile(path.join(__dirname+'/publications.html'));
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
